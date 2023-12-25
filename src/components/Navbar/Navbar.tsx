ddc1ec5d2dc1b8e0712a2e8d6d5f16de9edfc2b1
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2023-12-25 10:31:01 +0530

    Home slider added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,26 +2,33 @@ import React from "react";
 import logo from ".././../assets/logo.png";
 import Image from "next/image";
 import { FaRegCircleUser } from "react-icons/fa6";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <section>
+    <section className="bg-black/20">
       <nav>
-        <div className="w-11/12 mx-auto flex items-center justify-between">
+        <div className="w-11/12 mx-auto flex items-center justify-between ">
           {/* left part */}
-          <div className="flex items-center gap-3">
-            <Image src={logo} alt="image" />
-            <input type="search" />
+          <div className="flex items-center gap-6 p-3 ">
+            <Image src={logo} alt="image" className="w-[150px]" />
+            <input
+              type="search"
+              className="border-2 rounded-md bg-gray-200 p-1 "
+              placeholder="Search....."
+            />
           </div>
 
           {/*  right part  */}
 
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-6">
             <p>Location</p>
-            <button>Logout</button>
-            <i>
+            <button className="bg-[#f84464] px-3 py-2 text-white rounded-md">
+              Logout
+            </button>
+            <Link href=""  className="text-2xl">
               <FaRegCircleUser />
-            </i>
+            </Link>
           </div>
         </div>
       </nav>
